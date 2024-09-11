b26be4c2b3dc9840022fcf81bfefda0ab79cd2eb
greentimo <greentimo@myvuw.ac.nz>
2024-09-12 00:14:15 +1200

    Created non-animated cat bases

--- music_starter.js
+++ music_starter.js
@@ -1,6 +1,7 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
+  let catColours = [color("#e4c899"), color("#432a27"), color("#b38b6f"), color("#57433e")];
   let wallColour1 = color("#aa8453");
   let wallColour2 = color("#5b492f");
   let wallHeight = (height / 5) * 3;
@@ -28,53 +29,139 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   const extraHeight = 80;
 
   fill(wallColour2);
-  // left
-  quad(0, bottomHeight, 0, bottomHeight + extraHeight, cornerWidth, topHeight + extraHeight, cornerWidth, topHeight);
+  quad(0, bottomHeight, 0, bottomHeight + extraHeight, cornerWidth, topHeight + extraHeight, cornerWidth, topHeight); // left
   quad(0, wallHeight, 0, topHeight + extraHeight, cornerWidth, topHeight + extraHeight, cornerWidth, wallHeight);
-  // right
-  quad(width, bottomHeight, width, bottomHeight + extraHeight, width - cornerWidth, topHeight + extraHeight, width - cornerWidth, topHeight);
+  quad(width, bottomHeight, width, bottomHeight + extraHeight, width - cornerWidth, topHeight + extraHeight, width - cornerWidth, topHeight); // right
   quad(width, wallHeight, width, topHeight + extraHeight, width - cornerWidth, topHeight + extraHeight, width - cornerWidth, wallHeight);
 
   fill(wallColour1);
-  // left
-  triangle(0, bottomHeight, 0, topHeight, cornerWidth, topHeight);
-  // right
-  triangle(width, bottomHeight, width, topHeight, width - cornerWidth, topHeight);
+  triangle(0, bottomHeight, 0, topHeight, cornerWidth, topHeight); // left
+  triangle(width, bottomHeight, width, topHeight, width - cornerWidth, topHeight); // right
 
 
-  // switches which "cat" it draws depending on individual channel threshold
+  // switches which cat state it draws depending on individual channel threshold
   let vocalRange = map(vocal, 0, 100, 0, 20);
-  let bassRange = map(bass, 0, 100, 0, 20);
   let drumRange = map(drum, 0, 100, 0, 20);
+  let bassRange = map(bass, 0, 100, 0, 20);
   let otherRange = map(other, 0, 100, 0, 20);
 
-  if (vocalRange > 12) {
-    drawCat(width / 2, wallHeight * 1.4, color(200, 0, 0));
+  if (drumRange > 15) {
+    drawDrum(width / 2, wallHeight * 1, catColours);
   } else {
-    drawCat(width / 2, wallHeight * 1.4, color(0, 200, 0));
+    drawDrum(width / 2, wallHeight * 1, catColours);
+  }
+
+  if (vocalRange > 12) {
+    drawVocal(width / 2, wallHeight * 1.4, catColours);
+  } else {
+    drawVocal(width / 2, wallHeight * 1.4, catColours);
   }
 
   if (bassRange > 15.5) {
-    drawCat((width / 7) * 2.5, wallHeight * 1.3, color(0, 0, 200));
+    drawBass((width / 7) * 2.5, wallHeight * 1.3, catColours);
   } else {
-    drawCat((width / 7) * 2.5, wallHeight * 1.3, color(200, 200, 0));
-  }
-
-  if (drumRange > 15) {
-    drawCat(width / 2, wallHeight * 1.1, color(200, 0, 200));
-  } else {
-    drawCat(width / 2, wallHeight * 1.1, color(0, 200, 200));
+    drawBass((width / 7) * 2.5, wallHeight * 1.3, catColours);
   }
 
   if (otherRange > 16) {
-    drawCat((width / 7) * 4.5, wallHeight * 1.3, color(0, 0, 0));
+    drawOther((width / 7) * 4.5, wallHeight * 1.3, catColours);
   } else {
-    drawCat((width / 7) * 4.5, wallHeight * 1.3, color(200, 200, 200));
+    drawOther((width / 7) * 4.5, wallHeight * 1.3, catColours);
   }
 }
 
 
-function drawCat(x, y, colour) {
+function drawVocal(x, y, catColours) {
+  drawCatMain(x, y, catColours[0], catColours[1], catColours[2], catColours[3]);
+  drawCatHead(x, y, catColours[0]);
+}
+
+function drawDrum(x, y, catColours) {
+  drawCatMain(x, y, catColours[0], catColours[1], catColours[2], catColours[3]);
+  drawCatHead(x, y, catColours[0]);
+}
+
+function drawBass(x, y, catColours) {
+  drawCatMain(x, y, catColours[0], catColours[1], catColours[2], catColours[3]);
+  drawCatHead(x, y, catColours[0]);
+}
+
+function drawOther(x, y, catColours) {
+  drawCatMain(x, y, catColours[0], catColours[1], catColours[2], catColours[3]);
+  drawCatHead(x, y, catColours[0]);
+}
+
+// draws head of cat body
+function drawCatHead(x, y, colour) {
+  let headWidth = 90;
+  let headHeight = 70;
+
+  // main head
   fill(colour);
-  ellipse(x, y, 50, 50);
+  ellipse(x, y - 70, headWidth, headHeight);
+
+  // ears
+  quad(x + 7 - headWidth / 2, y - 54 - headHeight / 2, x - 32, y - 72 - headHeight / 2, 
+    x - 18, y - 76 - headHeight / 2, x - 8, y - 68 - headHeight / 2); // left
+  ellipse(x + 20.3 - headWidth / 2, y - 72.8 - headHeight / 2, 14, 12);
+  quad(x - 7 + headWidth / 2, y - 54 - headHeight / 2, x + 32, y - 72 - headHeight / 2, 
+    x + 18, y - 76 - headHeight / 2, x + 8, y - 68 - headHeight / 2); // right
+  ellipse(x - 20.3 + headWidth / 2, y - 72.8 - headHeight / 2, 14, 12);
+
+  // cheeks & chin
+  ellipse(x + 16 - headWidth / 2, y - 62, 40, 50); // left
+  ellipse(x - 16 + headWidth / 2, y - 62, 40, 50); // right
+  ellipse(x, y - 5 - headHeight / 2, 70, 15)
+}
+
+// draws non-animated parts of cat body
+function drawCatMain(x, y, colour1, colour2, colour3, colour4) {
+  let catWidth = 70;
+  let catHeight = 30;
+
+  // main body
+  fill(colour1);
+  ellipse(x, y, catWidth, catHeight);
+  ellipse(x + 12 - catWidth / 2, y - 2, 1 + catWidth / 3, catHeight * 2);
+  ellipse(x - 12 + catWidth / 2, y - 2, 1 + catWidth / 3, catHeight * 2);
+  quad(x - catWidth / 2, y - 10, x + catWidth / 2, y - 10, x + catWidth / 3, y - 20 - catHeight, x - catWidth / 3, y - 20 - catHeight);
+
+  // jacket
+  fill(colour4);
+  rect(x, y - 24, 30, 7);
+  fill(colour3);
+  quad(x - 5 - catWidth / 2, y - 15, x - 15, y - 10, x - 10, y - 20 - catHeight, x - catWidth / 3, y - 20 - catHeight); // left
+  push();
+  translate(x + 4 - catWidth / 2, y - 1 - catHeight);
+  rotate(24);
+  ellipse(0, 0, 10, 35);
+  pop();
+  quad(x + 5 + catWidth / 2, y - 15, x + 15, y - 10, x + 10, y - 20 - catHeight, x + catWidth / 3, y - 20 - catHeight); // right
+  push();
+  translate(x - 4 + catWidth / 2, y - 1 - catHeight);
+  rotate(-24);
+  ellipse(0, 0, 10, 35);
+  pop();
+
+  // legs
+  fill(colour1);
+  quad(x - 5, y + 7 + catHeight / 2, x + 5 - catWidth / 2, y + 7 + catHeight / 2, 
+    x - catWidth / 2, y, x, y); // left
+  quad(x + 5, y + 7 + catHeight / 2, x - 5 + catWidth / 2, y + 7 + catHeight / 2, 
+    x + catWidth / 2, y, x, y); // right
+
+  // hip things
+  fill(colour4);
+  quad(x - 1 - catWidth / 2, y + 1, x + 0.5 + catWidth / 2, y + 1, x + 1 + catWidth / 2, y - 6, x - 0.5 - catWidth / 2, y - 6);
+  fill(colour2);
+  triangle(x + 25, y, x - 1 - catWidth / 2, y, x + 2 - catWidth / 2, y + 3 + catHeight / 2); // left
+  triangle(x - 25, y, x + 1 + catWidth / 2, y, x - 2 + catWidth / 2, y + 3 + catHeight / 2); // right
+  
+  // boots
+  ellipse(x - catWidth / 4, y + 8 + catHeight, 19, 19); // left
+  quad(x - 10 - catWidth / 4, y + 8 + catHeight, x + 10 - catWidth / 4, y + 8 + catHeight, 
+    x - 5, y + 7 + catHeight / 2, x + 5 - catWidth / 2, y + 7 + catHeight / 2);
+  ellipse(x + catWidth / 4, y + 8 + catHeight, 19, 19); // right
+  quad(x + 10 + catWidth / 4, y + 8 + catHeight, x - 10 + catWidth / 4, y + 8 + catHeight, 
+    x + 5, y + 7 + catHeight / 2, x - 5 + catWidth / 2, y + 7 + catHeight / 2);
 }
